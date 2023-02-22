4e9d1b7d99ed30a31ba2007724df74c39f7a7fd9
Mina Isaac <mina.isaac.2000@gmail.com>
2023-02-22 13:44:45 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { BackendResponse, Person } from "./types";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [personArray, setPersonArray] = useState<Person[]>([]);
+  useEffect(() => {
+    axios
+      .get<BackendResponse>("https://randomuser.me/api/?results=10")
+      .then((res) => setPersonArray(res.data.results));
+  }, []);
+
+  const dateOfBirths = personArray
+    .filter((item) => item.dob.age > 30)
+    .map((item) => item.picture);
+  const images = dateOfBirths.map((item) => {
+    return (
+      <div style={{ borderRadius: "50%", padding: "20px" }}>
+        <img src={item.large} alt="صورة روح امه" />
+      </div>
+    );
+  });
+  return <div className="App">{images}</div>;
 }
 
 export default App;
