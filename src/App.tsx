24ca35cb424cff21a44ee0d9f619e016a93c1c6f
Mina Isaac <mina.isaac.2000@gmail.com>
2023-02-26 00:12:17 +0200

    Ramez changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import { BackendResponse, Person } from "./types";
+import { isTemplateExpression } from "typescript";
 
 function App() {
   const [personArray, setPersonArray] = useState<Person[]>([]);
+  const [hoveredItem, setHoveredItem] = useState<Person>();
+  const [card, setCard] = useState<any>();
+
   useEffect(() => {
     axios
       .get<BackendResponse>("https://randomuser.me/api/?results=10")
@@ -14,11 +18,23 @@ function App() {
 
   const dateOfBirths = personArray
     .filter((item) => item.dob.age > 30)
-    .map((item) => item.picture);
+    .map((item) => item);
+
   const images = dateOfBirths.map((item) => {
     return (
-      <div style={{ borderRadius: "50%", padding: "20px" }}>
-        <img src={item.large} alt="صورة روح امه" />
+      <div
+        style={{ borderRadius: "50%", padding: "20px", position: "relative" }}
+        onClick={() => setCard(item.name)}
+        onMouseEnter={() => {
+          setHoveredItem(item);
+        }}
+        onMouseLeave={() => setHoveredItem(undefined)}
+      >
+        {card === item.name && <div>{item.name.first}</div>}
+        <img src={item.picture.large} alt="صورة روح امه" />
+        {hoveredItem === item && (
+          <div className="name-container">{item.name.first}</div>
+        )}
       </div>
     );
   });
